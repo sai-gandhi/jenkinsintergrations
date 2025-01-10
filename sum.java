66dfdafe787cb06c9507a1742db49a98c34a3bd5
sai-gandhi <149860264+sai-gandhi@users.noreply.github.com>
2025-01-10 17:11:27 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,8 +4,8 @@ class Main {
         int a=5;
         int b=10;
         int sum=a+b;
-        System.out.println(a);
-        System.out.println(b);
+        System.out.println("A value is: "+a);
+        System.out.println("B value is: "+b);
         System.out.println("Sum of two numbers is: "+sum);
         System.out.println("Hello Sai Gandhi");
 
