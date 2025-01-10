59d2f70be5e26b0a478e5fb5fe37bef0f64fb453
sai-gandhi <149860264+sai-gandhi@users.noreply.github.com>
2025-01-10 17:04:42 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+class Main {
+
+  public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int sum=a+b;
+        System.out.println(sum);
+        System.out.println("Hello Sai Gandhi");
+
+  }
+}
